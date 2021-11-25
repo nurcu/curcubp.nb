b439769e57f63dc1606278275e3f8175e5b1088c
Loud Coypu <loud.coypu-code@yahoo.com>
2021-11-25 10:35:02 +0100

    App prepared for no react-bootstrap

--- packages/web-client/src/App.js
+++ packages/web-client/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Switch, Route, Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreatePosition from "./components/create-position.component";
+import EditPosition from "./components/edit-position.component";
+import PositionList from "./components/position-list.component";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            bezKoder
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+                <Route exact path={["/", "/positions"]} component={CreatePosition} />
+                <Route path="/create-position" component={CreatePosition} />
+                <Route path="/edit-position/:id" component={EditPosition} />
+                <Route path="/position-list" component={PositionList} />
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
